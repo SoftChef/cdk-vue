4740affdc742a86aca53295be06782405a82c93e
MinCheTsai <minche@softchef.com>
2021-10-04 12:36:58 +0800

    Upgrade projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism, NpmAccess } = require('projen');
+const { AwsCdkConstructLibrary, NpmAccess } = require('projen');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
@@ -27,13 +27,13 @@ const project = new AwsCdkConstructLibrary({
     '@vue/cli',
     'esbuild',
   ],
-  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+  depsUpgradeOptions: {
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
       secret: AUTOMATION_TOKEN,
     },
-  }),
+  },
   autoApproveOptions: {
     secret: 'GITHUB_TOKEN',
     allowedUsernames: ['MinCheTsai'],
