c6622aeeb200562f274d413813866da9df879161
MinCheTsai <minche@softchef.com>
2022-05-10 15:22:37 +0800

    Change docker dummy image

--- src/build.ts
+++ src/build.ts
@@ -51,7 +51,7 @@ export class VueCliBundling implements BundlingOptions {
     VueCliBundling.runsLocally = (getNpxVersion()?.startsWith(NPX_MAJOR_VERSION) && getVueCliVersion()?.startsWith('@vue/cli')) ?? false;
     const bundlingArguments = props.bundlingArguments ?? '';
     const bundlingCommand = this.createDockerBundlingCommand(AssetStaging.BUNDLING_OUTPUT_DIR, bundlingArguments);
-    this.image = VueCliBundling.runsLocally ? DockerImage.fromRegistry('dummy') : DockerImage.fromRegistry(`${props.nodeImage ?? 'public.ecr.aws/bitnami/node'}`);
+    this.image = VueCliBundling.runsLocally ? DockerImage.fromRegistry('williamyeh/dummy') : DockerImage.fromRegistry(`${props.nodeImage ?? 'public.ecr.aws/bitnami/node'}`);
     this.command = ['bash', '-c', bundlingCommand];
     this.environment = props.environment;
     if (!props.forceDockerBundling) {
